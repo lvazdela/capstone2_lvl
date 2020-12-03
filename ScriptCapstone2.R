584e056a699ffc3791dd9b1237f3b9f80a8d551d
lvazdela <lgvazdela@yahoo.com.mx>
2020-12-02 20:26:14 -0600

    upload ScriptCapstone2.R

---
+++ ScriptCapstone2.R
@@ -0,0 +1,17 @@
+#_____________________________________________
+# Movielens project for Edx capstone
+# Student: Luis Vazquez de Lara
+# Date: September nineth, 2020
+#______________________________________________
+
+library(tidyverse)
+library(lubridate)
+library(caret)
+
+coldef <- 'darkolivegreen4'
+filldef <- 'darkolivegreen2'
+
+
+# DATA WRANGING -----------------------------------------------------------
+
+
